82bba376576d47889f50beb4416241d92e747bf5
Denys Samoilenko <denniksam@gmail.com>
2023-04-14 20:55:23 +0300

    2048 -- 4

--- app/src/main/java/step/learning/course/GameActivity.java
+++ app/src/main/java/step/learning/course/GameActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +18,7 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity {
     private final int N = 4 ;
     private final int[][] cells = new int[N][N] ;  // значения в ячейках поля
+    private final int[][] saves = new int[N][N] ;  // предыдущий ход
     private final TextView[][] tvCells = new TextView[N][N] ;   // ссылки на ячеки поля
     private final Random random = new Random() ;
 
@@ -22,6 +26,8 @@ public class GameActivity extends AppCompatActivity {
     private int bestScore ;
     private TextView tvScore ;
     private TextView tvBestScore ;
+    private Animation spawnAnimation ;
+    private Animation collapseAnimation ;
 
     @SuppressLint("DiscouragedApi")
     @Override
@@ -33,6 +39,11 @@ public class GameActivity extends AppCompatActivity {
         tvBestScore = findViewById( R.id.game_tv_best_score ) ;
         tvScore.setText( getString( R.string.game_score, "69.6k" ) ) ;
         tvBestScore.setText( getString( R.string.game_best_score, "69.6k" ) ) ;
+
+        spawnAnimation = AnimationUtils.loadAnimation( this, R.anim.cell_spawn ) ;
+        spawnAnimation.reset() ;
+        collapseAnimation = AnimationUtils.loadAnimation( this, R.anim.cell_collapse ) ;
+        collapseAnimation.reset() ;
 
         for( int i = 0; i < N; ++i ) {
             for( int j = 0; j < N; ++j ) {
@@ -51,11 +62,20 @@ public class GameActivity extends AppCompatActivity {
                         new OnSwipeTouchListener( GameActivity.this ) {
                             @Override
                             public void onSwipeRight() {
-                                Toast.makeText(
-                                        GameActivity.this,
-                                        "Right",
-                                        Toast.LENGTH_SHORT)
-                                    .show();
+                                //saveField() ;  // [0204] -> [0024] -> no move -> [undo]
+                                if( canMoveRight() ) {
+                                    saveField() ;
+                                    moveRight() ;
+                                    spawnCell() ;
+                                    showField() ;
+                                }
+                                else {
+                                    Toast.makeText(
+                                                    GameActivity.this,
+                                                    "No Right Move",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
                             }
                             @Override
                             public void onSwipeLeft() {
@@ -67,10 +87,10 @@ public class GameActivity extends AppCompatActivity {
                             }
                         }
                 ) ;
-
-        newGame() ;
+        findViewById( R.id.game_new ).setOnClickListener( this::newGame ) ;
+        newGame( null ) ;
     }
-    private void newGame() {
+    private void newGame( View view ) {
         for( int i = 0; i < N; ++i ) {
             for( int j = 0; j < N; ++j ) {
                 cells[i][j] = 0 ;
@@ -80,6 +100,13 @@ public class GameActivity extends AppCompatActivity {
         spawnCell() ;
         spawnCell() ;
         showField() ;
+    }
+    private boolean canMoveRight() {
+        /*
+        Д.З. Реализовать проверку возможности хода вправо (без изменения состояния поля)
+        ** реализовать ходы и проверки по другим направлениям
+         */
+        return true ;
     }
     @SuppressLint("DiscouragedApi")
     private void showField() {
@@ -130,8 +157,62 @@ public class GameActivity extends AppCompatActivity {
         int y = coordinates.get( randIndex ).getY() ;
         // ставим в ячейку 2 / 4
         cells[x][y] = random.nextInt( 10 ) == 0 ? 4 : 2 ;
-
+        // проигрываем анимацию для появившейся ячейки
+        tvCells[x][y].startAnimation( spawnAnimation ) ;
         return true ;
+    }
+    private boolean moveRight() {
+        boolean isMoved = false ;    // [0002]->[0002], [0200]->[0002], [2020]->[0022]->[0004]
+
+        for( int i = 0; i < N; ++i ) {
+            // сдвиги
+            boolean wasReplace;
+            do {
+                wasReplace = false;
+                for (int j = N - 1; j > 0; --j) {
+                    if (cells[i][j] == 0          // текущая ячейка 0
+                            && cells[i][j - 1] != 0) {    // а перед ней - не 0
+                        cells[i][j] = cells[i][j - 1];
+                        cells[i][j - 1] = 0;
+                        wasReplace = true;
+                        isMoved = true;
+                    }
+                }
+            } while (wasReplace);
+
+            // collapse
+            for (int j = N - 1; j > 0; --j) {  // [2202] -> [0222] -> [0204] -> [0024]
+                if (cells[i][j] == cells[i][j - 1] && cells[i][j] != 0) {  // соседние ячейки равны  [2222]
+                    score += cells[i][j] + cells[i][j - 1] ;   // счет = сумма всех объединенных ячеек
+                    cells[i][j] *= -2 ;  // [2224]; "-" - признак для анимации
+                    cells[i][j - 1] = 0 ;   // [2204]
+                    isMoved = true ;
+                }
+            }  // [0404]  при коллапсе может понадобиться дополнительное смещение
+            for (int j = N - 1; j > 0; --j) {
+                if (cells[i][j] == 0 && cells[i][j - 1] != 0) {
+                    cells[i][j] = cells[i][j - 1];
+                    cells[i][j - 1] = 0;
+                }
+            }
+            for (int j = N - 1; j > 0; --j) {
+                if( cells[i][j] < 0 ) {  // надо включить анимацию
+                    cells[i][j] = -cells[i][j] ;
+                    tvCells[i][j].startAnimation( collapseAnimation ) ;
+                }
+            } // [0044]
+        }
+        return isMoved ;
+    }
+    private void saveField() {
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(cells[i], 0, saves[i], 0, N);
+        }
+    }
+    private void undoMove() {
+        for (int i = 0; i < N; i++) {
+            System.arraycopy(saves[i], 0, cells[i], 0, N);
+        }
     }
     /*
     Д.З. Провести рефакторинг spawnCell()
@@ -158,6 +239,19 @@ public class GameActivity extends AppCompatActivity {
     }
 }
 /*
-Д.З. Реализовать все свайпы
-Подобрать цвета для ячеек с различными значениями
+Анимации
+1. Создаем ресурсную папку res/anim (ПКМ - папка - anim)
+2. Создаем ресурсный файл cell_spawn.xml, корневой элемент alpha (см файл)
+3. Создаем переменную типа Animation spawnAnimation
+4. Загружаем анимацию
+    spawnAnimation = AnimationUtils.loadAnimation( this, R.anim.cell_spawn ) ;
+    spawnAnimation.reset() ;
+5. Проигрываем анимацию (на любом View)
+    tvCells[x][y].startAnimation( spawnAnimation ) ;
+
+Виды анимаций
+alpha - прозрачность
+rotate - вращение
+translate - перемещение
+scale - размер(масштаб)
  */
